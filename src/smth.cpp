18020daa090da2e9470acc4a2ebb4868bf280736
Yurevych Nataliia <116509687+yurevych2@users.noreply.github.com>
2025-03-26 22:07:25 +0200

    Create an empty project

---
+++ src/smth.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Administrator on 26.03.2025.
+//
+
+#include "smth.h"
---
+++ src/smth.h
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 26.03.2025.
+//
+
+#ifndef STEREO_VISION_DEPTH_MAP_SMTH_H
+#define STEREO_VISION_DEPTH_MAP_SMTH_H
+
+
+class smth {
+
+};
+
+
+#endif //STEREO_VISION_DEPTH_MAP_SMTH_H
